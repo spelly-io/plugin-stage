41ec70b1cc501c12d4f07b42aa977e2156407b84
Dmitriy Golub <dmitriy.golub@gmail.com>
2020-09-18 00:00:00 +0300

    Release 0.0.64

--- spelly.sketchplugin/Contents/Sketch/spellytools.framework/bootstrap.js
+++ spelly.sketchplugin/Contents/Sketch/spellytools.framework/bootstrap.js
@@ -1,17 +1,1 @@
-var onStartup = function (context) {
-  var ST_FrameworkPath = ST_FrameworkPath || COScript.currentCOScript().env().scriptURL.path().stringByDeletingLastPathComponent().stringByDeletingLastPathComponent();
-  var ST_Log = console.log || ST_Log || log;
-  (function () {
-    var mocha = Mocha.sharedRuntime();
-    var frameworkName = "SpellyTools";
-    var directory = ST_FrameworkPath;
-    if (mocha.valueForKey(frameworkName)) {
-      return true;
-    } else if (mocha.loadFrameworkWithName_inDirectory_(frameworkName, directory)) {
-      mocha.setValue_forKey_(true, frameworkName);
-      return true;
-    } else {
-      return false;
-    }
-  })();
-};
+var onStartup=function(e){var o,t,l=l||COScript.currentCOScript().env().scriptURL.path().stringByDeletingLastPathComponent().stringByDeletingLastPathComponent(),r=console.log||r||log;t=l,(o=Mocha.sharedRuntime()).valueForKey("SpellyTools")||o.loadFrameworkWithName_inDirectory_("SpellyTools",t)&&o.setValue_forKey_(!0,"SpellyTools")};
